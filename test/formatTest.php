fc55696ef4a4aad8fb8685ca6139fe429f584911
Nullivex <contact@nullivex.com>
2013-03-13 13:29:40 -0700

    added tests for all the functions

---
+++ test/formatTest.php
@@ -0,0 +1,14 @@
+<?php
+require_once(dirname(__DIR__).'/test_common.php');
+
+class FuncFormatTest extends PHPUNIT_Framework_TestCase {
+
+	public function testFormatBytes(){
+		$this->assertEquals('1,000KB',format_bytes(1000000));
+	}
+
+	public function testFormatIP(){
+		$this->assertEquals('001.002.003.004',format_ip('1.2.3.4',0,false));
+	}
+
+}
